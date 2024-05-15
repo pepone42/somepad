95888873f5ec3754254c3fef428b5360a628752f
Mathieu PEPONAS <MPEPONAS@bt0d0000.w2k.bouyguestelecom.fr>
2024-05-15 17:47:01 +0200

    clean up

--- ui/cushy/src/main.rs
+++ ui/cushy/src/main.rs
@@ -3,47 +3,31 @@ mod shortcut;
 mod scroll;
 mod settings;
 
-use std::any::Any;
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::default;
-use std::ops::Deref;
-use std::os::raw;
-use std::process::Child;
 use std::sync::{Arc, Mutex};
 
 use cushy::context::EventContext;
 use cushy::figures::units::{self, Lp, Px, UPx};
-use cushy::figures::{
-    Abs, FloatConversion, IntoSigned, Point, Px2D, Rect, Roots, Round, ScreenScale, Size, Zero,
-};
+use cushy::figures::{Abs, FloatConversion, Point, Rect, Round, ScreenScale, Size, Zero};
 use cushy::kludgine::app::winit::event::ElementState;
 use cushy::kludgine::app::winit::keyboard::{Key, NamedKey};
-use cushy::kludgine::cosmic_text::fontdb::ID;
-use cushy::kludgine::cosmic_text::{Attrs, Buffer, Cursor, Family, FontSystem, LayoutRun, Metrics};
-use cushy::kludgine::image::buffer;
-use cushy::kludgine::shapes::{Path, PathBuilder, Shape};
-use cushy::kludgine::text::{MeasuredText, Text};
+use cushy::kludgine::cosmic_text::{Attrs, Buffer, Cursor, Family, FontSystem, Metrics};
+use cushy::kludgine::shapes::Shape;
 use cushy::kludgine::{Drawable, DrawableExt};
 use cushy::styles::components::CornerRadius;
-use cushy::styles::{Color, CornerRadii, Dimension, FamilyOwned, VisualOrder};
-use cushy::value::{
-    Destination, Dynamic, IntoDynamic, IntoReadOnly, IntoValue, Source, Switchable,
-};
+use cushy::styles::{Color, CornerRadii, Dimension};
+use cushy::value::{Destination, Dynamic, Source, Switchable};
 use cushy::widget::{
-    self, EventHandling, MakeWidget, MakeWidgetList, MakeWidgetWithTag, Widget, WidgetId,
-    WidgetInstance, WidgetRef, WidgetTag, WrapperWidget, HANDLED, IGNORED,
+    EventHandling, MakeWidget, Widget, WidgetId, WidgetRef, WrapperWidget, HANDLED, IGNORED,
 };
-use cushy::widgets::input::{self, InputValue};
-use cushy::widgets::layers::{OverlayLayer, Overlayable};
-use cushy::widgets::{color, Custom, Data, Input, Layers, Resize, Switcher};
+
+use cushy::widgets::{Custom, Input};
 use cushy::window::KeyEvent;
 use cushy::{context, Lazy, ModifiersExt, Run};
 use ndoc::{rope_utils, Document};
 use scroll::{MyScroll, ScrollController};
 use settings::Settings;
 use shortcut::{event_match, Shortcut};
-use smol_str::SmolStr;
 
 #[derive(Debug)]
 pub struct TextEditor {
@@ -61,7 +45,6 @@ impl TextEditor {
     ) -> usize {
         // TODO: tab support
         let raw_text = doc.rope.line(line).to_string();
-        //cushy::kludgine::cosmic_text::Buffer::new(font_system, Metrics::new(font_size, line_height))
         let mut buffer = Buffer::new(font_system, Metrics::new(15.0, 20.0));
         buffer.set_size(font_system, 10000., 20.);
         buffer.set_text(
@@ -85,7 +68,6 @@ impl TextEditor {
     ) -> Px {
         // TODO: tab support
         let raw_text = doc.rope.line(line).to_string();
-        //cushy::kludgine::cosmic_text::Buffer::new(font_system, Metrics::new(font_size, line_height))
         let mut buffer = Buffer::new(font_system, Metrics::new(15.0, 20.0));
         buffer.set_size(font_system, 1000., 1000.);
         buffer.set_text(
@@ -151,12 +133,13 @@ impl Widget for TextEditor {
             let y = units::Px::new(i as _) * 20;
             let slice = doc.rope.slice(..);
             let raw_text = ndoc::rope_utils::get_line_info(&slice, i as _, 4);
-            let attrs = Attrs::new().family(Family::Monospace); //.color(cushy::kludgine::cosmic_text::Color::rgba(0xff,0xff,0xff,0xdd));
+            let attrs = Attrs::new().family(Family::Monospace);
 
             context.gfx.set_text_attributes(attrs);
 
             if let Some(sl) = doc.get_style_line_info(i as _) {
-                let mut buffer = Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), Metrics::new(15.0, 20.0));
+                let mut buffer =
+                    Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), Metrics::new(15.0, 20.0));
                 let mut spans = Vec::new();
                 for s in sl.iter() {
                     let t = &raw_text[s.range.start..s.range.end];
@@ -177,10 +160,6 @@ impl Widget for TextEditor {
                     cushy::kludgine::cosmic_text::Shaping::Advanced,
                 );
                 buffer.set_size(&mut FONT_SYSTEM.lock().unwrap(), 10000., 20.);
-
-                // context.gfx.draw_text(
-                //     Text::new(&i.to_string(), Color::WHITE).translate_by(Point::new(-translation.x, y)),
-                // );
 
                 context.gfx.draw_text_buffer(
                     Drawable {
@@ -203,7 +182,6 @@ impl Widget for TextEditor {
             .iter()
             .filter(|s| s.head.line >= first_line && s.head.line < last_line)
         {
-            //dbg!(s.start(), s.end());
             let head = TextEditor::grapheme_to_point(
                 &doc,
                 s.head.line,
@@ -211,7 +189,6 @@ impl Widget for TextEditor {
                 &mut FONT_SYSTEM.lock().unwrap(),
             )
             .floor();
-            //dbg!(start, end);
 
             context.gfx.draw_shape(
                 Shape::filled_rect(
@@ -224,72 +201,31 @@ impl Widget for TextEditor {
                 .translate_by(Point::new(head, Px::new(s.head.line as i32 * 20))),
             );
         }
-
-        // for i in first_line.get()..last_line.get() {
-        //     let y = units::Px::new(i as _) * 20;
-        //     let slice = doc.rope.slice(..);
-        //     let raw_text = ndoc::rope_utils::get_line_info(&slice, i as _, 4);
-        //     let attrs = Attrs::new().family(Family::Monospace); //.color(cushy::kludgine::cosmic_text::Color::rgba(0xff,0xff,0xff,0xdd));
-
-        //     context.gfx.set_text_attributes(attrs);
-
-        //     if let Some(sl) = doc.get_style_line_info(i as _) {
-        //         let mut x = units::Px::ZERO;
-        //         for s in sl.iter() {
-        //             let t = &raw_text[s.range.start..s.range.end];
-        //             let col = Color::new(
-        //                 s.style.foreground.r,
-        //                 s.style.foreground.g,
-        //                 s.style.foreground.b,
-        //                 s.style.foreground.a,
-        //             );
-        //             let t = Text::<units::Px>::new(t, col);
-        //             let m = context.gfx.measure_text(t);
-
-        //             context.gfx.draw_measured_text(
-        //                 m.translate_by(Point::new(x, y)),
-        //                 cushy::kludgine::text::TextOrigin::TopLeft,
-        //             );
-        //             x += m.size.width;
-        //             if i == 1 {
-        //                 dbg!(
-        //                     m.size.width,
-        //                     m.glyphs
-        //                         .iter()
-        //                         .fold(Px::ZERO, |acc, g| acc + g.info.line_width)
-        //                 );
-        //             }
-        //         }
-        //     }
-        // }
     }
 
     fn layout(
         &mut self,
-        available_space: Size<cushy::ConstraintLimit>,
+        _available_space: Size<cushy::ConstraintLimit>,
         context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
     ) -> Size<UPx> {
         let height = self.doc.get().rope.len_lines() as f32 * 20.0;
 
-        //dbg!(context.gfx.translation());
         self.viewport.set(Rect::new(
             context.gfx.translation().abs(),
             context.gfx.size().into_px(context.gfx.scale()),
         ));
-        // dbg!("qsdfqsdf");
-        // //self.refocus_main_selection(&mut FONT_SYSTEM.lock().unwrap());
 
-        Size::new(UPx::new(1000), UPx::new(height.ceil() as _))
+        Size::new(UPx::new(10000), UPx::new(height.ceil() as _))
     }
 
-    fn accept_focus(&mut self, context: &mut cushy::context::EventContext<'_>) -> bool {
+    fn accept_focus(&mut self, _context: &mut cushy::context::EventContext<'_>) -> bool {
         true
     }
 
     fn hit_test(
         &mut self,
-        location: Point<units::Px>,
-        context: &mut cushy::context::EventContext<'_>,
+        _location: Point<units::Px>,
+        _context: &mut cushy::context::EventContext<'_>,
     ) -> bool {
         true
     }
@@ -297,8 +233,8 @@ impl Widget for TextEditor {
     fn mouse_down(
         &mut self,
         location: Point<units::Px>,
-        device_id: cushy::window::DeviceId,
-        button: cushy::kludgine::app::winit::event::MouseButton,
+        _device_id: cushy::window::DeviceId,
+        _button: cushy::kludgine::app::winit::event::MouseButton,
         context: &mut cushy::context::EventContext<'_>,
     ) -> EventHandling {
         if !context.enabled() {
@@ -324,9 +260,9 @@ impl Widget for TextEditor {
 
     fn keyboard_input(
         &mut self,
-        device_id: cushy::window::DeviceId,
+        _device_id: cushy::window::DeviceId,
         input: cushy::window::KeyEvent,
-        is_synthetic: bool,
+        _is_synthetic: bool,
         context: &mut cushy::context::EventContext<'_>,
     ) -> cushy::widget::EventHandling {
         if !context.enabled() {
@@ -436,30 +372,16 @@ pub struct Palette {
 impl Palette {
     pub fn new() -> Self {
         let input = Dynamic::new(String::default());
-        let child = "test"
-            .and(
-                Custom::new(
-                    Input::new(input.clone()), // .on_key(move |k| {
-                                               //     match dbg!(k).logical_key {
-                                               //         Key::Named(NamedKey::Enter) => {
-                                               //             if let Some(pa) = pa.get() {
-                                               //                 (pa)(0, input.get().clone());
-                                               //             }
-                                               //             //pr.set(PaletteResult::Value(input.get().clone()));
-                                               //             pal.set(false);
-                                               //             HANDLED
-                                               //         }
-                                               //         _ => IGNORED,
-                                               //     }
-                                               // })
-                )
-                .on_mounted(move |c| c.focus()),
-            )
-            .into_rows()
+        let pal = "palette"
+            .and(Custom::new(Input::new(input.clone())).on_mounted(move |c| c.focus()))
+            .into_rows().size(Size::new(Px::new(200), Px::new(200))).background_color(Color::new(0x34, 0x3D, 0x46, 0xFF))
             .centered()
             .align_top();
+
+        //let child = Custom::empty().size(Size::new(width, height))
+
         Palette {
-            child: child.make_widget().widget_ref(),
+            child: pal.make_widget().widget_ref(),
             action: PALETTE_ACTION.clone(),
             input,
         }
@@ -482,9 +404,9 @@ impl WrapperWidget for Palette {
 
     fn keyboard_input(
         &mut self,
-        device_id: cushy::window::DeviceId,
+        _device_id: cushy::window::DeviceId,
         input: KeyEvent,
-        is_synthetic: bool,
+        _is_synthetic: bool,
         context: &mut context::EventContext<'_>,
     ) -> EventHandling {
         match input.logical_key {
@@ -515,21 +437,7 @@ pub enum PaletteResult {
 pub struct EditorWindow {
     child: WidgetRef,
     documents: Dynamic<Vec<Dynamic<Document>>>,
-    // palette: Dynamic<bool>,
-    // palette_action: Dynamic<Option<Arc<dyn Fn(usize, String) + 'static + Send + Sync>>>,
-    // palette_result: Dynamic<PaletteResult>,
 }
-
-// impl std::fmt::Debug for EditorWindow {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         f.debug_struct("EditorWindow")
-//             .field("child", &self.child)
-//             .field("documents", &self.documents)
-//             .field("palette", &self.palette)
-//             .field("palette_result", &self.palette_result)
-//             .finish()
-//     }
-// }
 
 static PALETTE: Lazy<Dynamic<bool>> = Lazy::new(|| Dynamic::new(false));
 static PALETTE_ACTION: Lazy<
@@ -554,33 +462,22 @@ impl EditorWindow {
 
         let child = child.make_widget();
         let child_id = child.id();
-        let input = Dynamic::new(String::new());
-        let palette_result = Dynamic::new(PaletteResult::None);
-        let palette_action = PALETTE_ACTION.clone();
-        let pr = palette_result.clone();
-        let pal = palette.clone();
-        let pa = palette_action.clone();
 
         let w = child
             .with_enabled(enabled)
-            .and(palette.clone().switcher(move |current, active| {
+            .and(palette.clone().switcher(move |current, _active| {
                 if *current {
-                    let pr = pr.clone();
-                    let input = input.clone();
-                    let pal = pal.clone();
-                    let pa = pa.clone();
-
                     Palette::new().make_widget()
                 } else {
-                    match pr.clone().get() {
-                        PaletteResult::Value(v) => {
-                            dbg!(v);
-                        }
-                        PaletteResult::ListItem(i) => {
-                            dbg!(i);
-                        }
-                        PaletteResult::None => (),
-                    }
+                    // match pr.clone().get() {
+                    //     PaletteResult::Value(v) => {
+                    //         dbg!(v);
+                    //     }
+                    //     PaletteResult::ListItem(i) => {
+                    //         dbg!(i);
+                    //     }
+                    //     PaletteResult::None => (),
+                    // }
                     Custom::empty()
                         .on_mounted(move |c| c.for_other(&child_id).unwrap().focus())
                         .make_widget()
@@ -590,9 +487,6 @@ impl EditorWindow {
         EditorWindow {
             child: w.widget_ref(),
             documents: Dynamic::new(Vec::new()),
-            // palette,
-            // palette_action,
-            // palette_result: palette_result.clone(),
         }
     }
 
@@ -608,9 +502,9 @@ impl WrapperWidget for EditorWindow {
 
     fn keyboard_input(
         &mut self,
-        device_id: cushy::window::DeviceId,
+        _device_id: cushy::window::DeviceId,
         input: KeyEvent,
-        is_synthetic: bool,
+        _is_synthetic: bool,
         context: &mut cushy::context::EventContext<'_>,
     ) -> EventHandling {
         if input.state == ElementState::Pressed && context.modifiers().possible_shortcut() {
@@ -672,9 +566,8 @@ const GOTO_LINE: ViewCommand = ViewCommand {
         ask(id, move |c, _, s| {
             if let Ok(line) = s.parse() {
                 {
-                    let mut doc = doc.lock();
                     let p = ndoc::Position::new(line, 0);
-                    doc.set_main_selection(p, p);
+                    doc.lock().set_main_selection(p, p);
                 }
                 c.widget()
                     .lock()
