10f012d7e08529bfb9b31aa454c166eae5b32a03
Mathieu PEPONAS <MPEPONAS@bt0d0000.w2k.bouyguestelecom.fr>
2024-05-17 14:43:16 +0200

    add components for editor text size and line height

--- ui/cushy/src/widgets/text_editor.rs
+++ ui/cushy/src/widgets/text_editor.rs
@@ -3,7 +3,7 @@ use cushy::styles::components::{CornerRadius, SurfaceColor};
 use cushy::value::Dynamic;
 
 use cushy::figures::units::{self, Lp, Px, UPx};
-use cushy::figures::{Abs, FloatConversion, Point, Rect, Round, ScreenScale, Size, Zero};
+use cushy::figures::{Abs, FloatConversion, Fraction, Point, Rect, Round, ScreenScale, Size, Zero};
 use cushy::kludgine::app::winit::event::ElementState;
 use cushy::kludgine::app::winit::keyboard::{Key, NamedKey};
 use cushy::kludgine::cosmic_text::{Attrs, Buffer, Cursor, Family, FontSystem, Metrics};
@@ -12,9 +12,12 @@ use cushy::kludgine::{Drawable, DrawableExt};
 
 use cushy::styles::{Color, CornerRadii, Dimension};
 use cushy::value::{Destination, Source};
-use cushy::widget::{EventHandling, MakeWidget, MakeWidgetWithTag, Widget, WidgetId, WidgetTag, WrapperWidget, HANDLED, IGNORED};
+use cushy::widget::{
+    EventHandling, MakeWidget, MakeWidgetWithTag, Widget, WidgetId, WidgetTag, WrapperWidget,
+    HANDLED, IGNORED,
+};
 
-use cushy::ModifiersExt;
+use cushy::{define_components, ModifiersExt};
 use ndoc::{rope_utils, Document};
 use scroll::ScrollController;
 
@@ -28,6 +31,10 @@ pub struct TextEditor {
     pub doc: Dynamic<ndoc::Document>,
     viewport: Dynamic<Rect<Px>>,
     scroll_controller: Dynamic<ScrollController>,
+    font_metrics: Metrics,
+    font_size: Px,
+    line_height: Px,
+    scale: Fraction,
 }
 
 impl TextEditor {
@@ -36,6 +43,10 @@ impl TextEditor {
             doc,
             viewport: Dynamic::new(Rect::default()),
             scroll_controller: Dynamic::new(ScrollController::default()),
+            font_metrics: Default::default(),
+            font_size: Px::ZERO,
+            line_height: Px::ZERO,
+            scale: Fraction::ZERO,
         }
     }
 
@@ -44,18 +55,17 @@ impl TextEditor {
         self
     }
 
-    fn point_to_grapheme(
-        doc: &ndoc::Document,
-        line: usize,
-        point: Point<Px>,
-        font_system: &mut FontSystem,
-    ) -> usize {
+    fn point_to_grapheme(&self, line: usize, point: Point<Px>) -> usize {
         // TODO: tab support
-        let raw_text = doc.rope.line(line).to_string();
-        let mut buffer = Buffer::new(font_system, Metrics::new(15.0, 20.0));
-        buffer.set_size(font_system, 10000., 20.);
+        let raw_text = self.doc.get().rope.line(line).to_string();
+        let mut buffer = Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), self.font_metrics);
+        buffer.set_size(
+            &mut FONT_SYSTEM.lock().unwrap(),
+            10000.,
+            self.font_metrics.line_height,
+        );
         buffer.set_text(
-            font_system,
+            &mut FONT_SYSTEM.lock().unwrap(),
             &raw_text,
             Attrs::new().family(Family::Monospace),
             cushy::kludgine::cosmic_text::Shaping::Advanced,
@@ -64,29 +74,28 @@ impl TextEditor {
             .hit(point.x.into_float(), point.y.into_float())
             .unwrap_or_default()
             .index;
-        rope_utils::byte_to_grapheme(&doc.rope.line(line as _), byte_idx)
+        rope_utils::byte_to_grapheme(&self.doc.get().rope.line(line as _), byte_idx)
     }
 
-    fn grapheme_to_point(
-        doc: &ndoc::Document,
-        line: usize,
-        index: usize,
-        font_system: &mut FontSystem,
-    ) -> Px {
+    fn grapheme_to_point(&self, line: usize, index: usize) -> Px {
         // TODO: tab support
-        let raw_text = doc.rope.line(line).to_string();
-        let mut buffer = Buffer::new(font_system, Metrics::new(15.0, 20.0));
-        buffer.set_size(font_system, 1000., 1000.);
+        let raw_text = self.doc.get().rope.line(line).to_string();
+        let mut buffer = Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), self.font_metrics);
+        buffer.set_size(
+            &mut FONT_SYSTEM.lock().unwrap(),
+            10000.,
+            self.font_metrics.line_height,
+        );
         buffer.set_text(
-            font_system,
+            &mut FONT_SYSTEM.lock().unwrap(),
             &raw_text,
             Attrs::new().family(Family::Monospace),
             cushy::kludgine::cosmic_text::Shaping::Advanced,
         );
-        let col = rope_utils::grapheme_to_byte(&doc.rope.line(line), index);
+        let col = rope_utils::grapheme_to_byte(&self.doc.get().rope.line(line), index);
         let c_start = Cursor::new(0, col);
         let c_end = Cursor::new(0, col + 1);
-        buffer.line_layout(font_system, 0);
+        buffer.line_layout(&mut FONT_SYSTEM.lock().unwrap(), 0);
         buffer
             .layout_runs()
             .nth(0)
@@ -99,18 +108,18 @@ impl TextEditor {
 
     pub fn refocus_main_selection(&self) {
         if self.doc.get().selections.len() == 1 {
-            let main_selection_head_x = TextEditor::grapheme_to_point(
-                &self.doc.get(),
+            let main_selection_head_x = self.grapheme_to_point(
                 self.doc.get().selections[0].head.line,
                 self.doc.get().selections[0].head.column,
-                &mut FONT_SYSTEM.lock().unwrap(),
             );
             self.scroll_controller.lock().make_region_visible(Rect::new(
                 Point::new(
                     Px::ZERO + main_selection_head_x - 10,
-                    Px::ZERO + Px::new(self.doc.get().selections[0].head.line as i32 * 20) - 10,
+                    Px::ZERO
+                        + Px::new(self.doc.get().selections[0].head.line as i32) * self.line_height
+                        - 10,
                 ),
-                Size::new(Px::new(35), Px::new(40)),
+                Size::new(Px::new(35), self.line_height + 20),
             ));
         }
     }
@@ -118,7 +127,7 @@ impl TextEditor {
 
 impl Widget for TextEditor {
     fn redraw(&mut self, context: &mut cushy::context::GraphicsContext<'_, '_, '_, '_>) {
-        let first_line = (-context.gfx.translation().y / 20.0) -1;
+        let first_line = (-context.gfx.translation().y / self.line_height) - 1;
         let last_line = first_line
             + (context
                 .gfx
@@ -126,7 +135,8 @@ impl Widget for TextEditor {
                 .size
                 .height
                 .into_px(context.gfx.scale())
-                / 20.0) + 1;
+                / self.line_height)
+            + 1;
 
         let first_line = first_line.get().max(0) as usize;
         let last_line = last_line.get() as usize;
@@ -137,7 +147,7 @@ impl Widget for TextEditor {
         let doc = self.doc.get_tracking_redraw(context);
 
         for i in first_line..last_line {
-            let y = units::Px::new(i as _) * 20;
+            let y = units::Px::new(i as _) * self.line_height;
             let slice = doc.rope.slice(..);
             let raw_text = ndoc::rope_utils::get_line_info(&slice, i as _, 4);
             let attrs = Attrs::new().family(Family::Monospace);
@@ -145,8 +155,7 @@ impl Widget for TextEditor {
             context.gfx.set_text_attributes(attrs);
 
             if let Some(sl) = doc.get_style_line_info(i as _) {
-                let mut buffer =
-                    Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), Metrics::new(15.0, 20.0));
+                let mut buffer = Buffer::new(&mut FONT_SYSTEM.lock().unwrap(), self.font_metrics);
                 let mut spans = Vec::new();
                 for s in sl.iter() {
                     let t = &raw_text[s.range.start..s.range.end];
@@ -166,7 +175,11 @@ impl Widget for TextEditor {
                     attrs,
                     cushy::kludgine::cosmic_text::Shaping::Advanced,
                 );
-                buffer.set_size(&mut FONT_SYSTEM.lock().unwrap(), 10000., 20.);
+                buffer.set_size(
+                    &mut FONT_SYSTEM.lock().unwrap(),
+                    10000.,
+                    self.font_metrics.line_height,
+                );
 
                 context.gfx.draw_text_buffer(
                     Drawable {
@@ -187,26 +200,37 @@ impl Widget for TextEditor {
         for s in doc
             .selections
             .iter()
-            .filter(|s| s.head.line >= first_line && s.head.line < last_line)
+            .filter(|s| s.head.line >= first_line && s.head.line <= last_line)
         {
-            let head = TextEditor::grapheme_to_point(
-                &doc,
-                s.head.line,
-                s.head.column,
-                &mut FONT_SYSTEM.lock().unwrap(),
-            )
-            .floor();
+            let head = self.grapheme_to_point(s.head.line, s.head.column).floor();
 
             context.gfx.draw_shape(
                 Shape::filled_rect(
                     Rect::new(
                         Point::new(Px::ZERO, Px::ZERO),
-                        Size::new(Px::new(1), Px::new(20)),
+                        Size::new(Px::new(1), self.line_height),
                     ),
                     Color::WHITE,
                 )
-                .translate_by(Point::new(head, Px::new(s.head.line as i32 * 20))),
+                .translate_by(Point::new(
+                    head,
+                    Px::new(s.head.line as i32) * self.line_height,
+                )),
             );
+
+            // let main_selection_head_x = self.grapheme_to_point(
+            //     s.head.line,
+            //     s.head.column,
+            // );
+            // context.gfx.draw_shape(Shape::stroked_rect(Rect::new(
+            //     Point::new(
+            //         Px::ZERO + main_selection_head_x - 10,
+            //         Px::ZERO
+            //             + Px::new(s.head.line as i32) * self.line_height
+            //             - 10,
+            //     ),
+            //     Size::new(Px::new(35), self.line_height + 20),
+            // ),Color::WHITE).translate_by(Point::ZERO));
         }
     }
 
@@ -215,7 +239,14 @@ impl Widget for TextEditor {
         _available_space: Size<cushy::ConstraintLimit>,
         context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
     ) -> Size<UPx> {
-        let height = self.doc.get().rope.len_lines() as f32 * 20.0;
+        if context.gfx.scale() != self.scale {
+            self.scale = context.gfx.scale();
+            self.line_height = context.get(&LineHeight).into_px(context.gfx.scale()).ceil();
+            self.font_size = context.get(&TextSize).into_px(context.gfx.scale()).ceil();
+            self.font_metrics =
+                Metrics::new(self.font_size.into_float(), self.line_height.into_float());
+        }
+        let height = self.doc.get().rope.len_lines() as f32 * self.font_metrics.line_height;
 
         self.viewport.set(Rect::new(
             context.gfx.translation().abs(),
@@ -249,16 +280,11 @@ impl Widget for TextEditor {
         }
         context.focus();
 
-        let line = ((self.viewport.get().origin.y + location.y) / 20)
+        let line = ((self.viewport.get().origin.y + location.y) / self.line_height)
             .floor()
             .get();
 
-        let char_idx = TextEditor::point_to_grapheme(
-            &self.doc.get(),
-            line as _,
-            Point::new(location.x, 1.into()),
-            context.kludgine.font_system(),
-        );
+        let char_idx = self.point_to_grapheme(line as _, Point::new(location.x, 1.into()));
         let col = rope_utils::byte_to_grapheme(&self.doc.get().rope.line(line as _), char_idx);
         dbg!(line, char_idx, col);
 
@@ -347,9 +373,8 @@ impl Widget for TextEditor {
                     return HANDLED;
                 }
                 Key::Named(NamedKey::Enter) => {
-                    let mut doc = self.doc.lock();
-                    let linefeed = doc.file_info.linefeed.to_string();
-                    doc.insert(&linefeed);
+                    let linefeed = self.doc.get().file_info.linefeed.to_string();
+                    self.doc.lock().insert(&linefeed);
                     self.refocus_main_selection();
                     return HANDLED;
                 }
@@ -367,23 +392,45 @@ impl Widget for TextEditor {
             _ => IGNORED,
         }
     }
+    fn full_control_redraw(&self) -> bool {
+        true
+    }
 }
 
 #[derive(Debug)]
 pub struct Gutter {
     doc: Dynamic<Document>,
     scroller: Dynamic<ScrollController>,
+    font_metrics: Metrics,
+    font_size: Px,
+    line_height: Px,
+    scale: Fraction,
 }
 
 impl Gutter {
     pub fn new(doc: Dynamic<Document>, scroller: Dynamic<ScrollController>) -> Self {
-        Self { doc, scroller}
+        Self {
+            doc,
+            scroller,
+            font_metrics: Default::default(),
+            font_size: Px::ZERO,
+            line_height: Px::ZERO,
+            scale: Fraction::ZERO,
+        }
     }
 }
 
 impl Widget for Gutter {
+    fn mounted(&mut self, context: &mut cushy::context::EventContext<'_>) {
+        self.line_height = context.get(&LineHeight).into_px(context.kludgine.scale());
+        self.font_size = context.get(&TextSize).into_px(context.kludgine.scale());
+        self.font_metrics =
+            Metrics::new(self.font_size.into_float(), self.line_height.into_float());
+        self.font_metrics =
+            Metrics::new(self.font_size.into_float(), self.line_height.into_float());
+    }
     fn redraw(&mut self, context: &mut cushy::context::GraphicsContext<'_, '_, '_, '_>) {
-        let first_line = (-self.scroller.get().scroll().y / 20.0)-1;
+        let first_line = (-self.scroller.get().scroll().y / self.font_metrics.line_height) - 1;
         let last_line = first_line
             + (context
                 .gfx
@@ -391,28 +438,49 @@ impl Widget for Gutter {
                 .size
                 .height
                 .into_px(context.gfx.scale())
-                / 20.0) + 1;
+                / self.font_metrics.line_height)
+            + 1;
 
         let first_line = first_line.get().max(0) as usize;
-        let last_line = last_line.get() as usize;
+        let last_line = (last_line.get() as usize).min(self.doc.get().rope.len_lines());
 
-        context.gfx.set_font_size(Lp::points(12));
+        context
+            .gfx
+            .set_font_size(Px::new(self.font_metrics.font_size.ceil() as _));
 
         context.fill(Color::new(0x34, 0x3D, 0x46, 0xFF));
-        let doc = self.doc.get_tracking_redraw(context);
 
         for i in first_line..last_line {
-            let y = self.scroller.get().scroll().y + (units::Px::new(i as _) * 20);
-            let slice = doc.rope.slice(..);
-            let raw_text = ndoc::rope_utils::get_line_info(&slice, i as _, 4);
+            let y = self.scroller.get().scroll().y
+                + (units::Px::new(i as _) * self.font_metrics.line_height);
+
             let attrs = Attrs::new().family(Family::Monospace);
 
             context.gfx.set_text_attributes(attrs);
 
             context.gfx.draw_text(
-                Text::new(&format!(" {} ", i+1), Color::WHITE).translate_by(Point::new(Px::ZERO, y)),
+                Text::new(&format!(" {} ", i + 1), Color::WHITE)
+                    .translate_by(Point::new(Px::ZERO, y)),
             );
         }
+    }
+    fn layout(
+        &mut self,
+        available_space: Size<cushy::ConstraintLimit>,
+        context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
+    ) -> Size<UPx> {
+        if context.gfx.scale() != self.scale {
+            self.scale = context.gfx.scale();
+            self.line_height = context.get(&LineHeight).into_px(context.gfx.scale()).ceil();
+            self.font_size = context.get(&TextSize).into_px(context.gfx.scale()).ceil();
+            self.font_metrics =
+                Metrics::new(self.font_size.into_float(), self.line_height.into_float());
+        }
+        // I don't understand why the +1 is needed. Without it, the gutter is too short by 1pixel vs the text editor
+        Size::new(UPx::new(50), available_space.height.max() + 1) 
+    }
+    fn full_control_redraw(&self) -> bool {
+        true
     }
 }
 
@@ -424,27 +492,28 @@ pub struct CodeEditor {
 
 impl CodeEditor {
     pub fn new(doc: Dynamic<Document>) -> Self {
-        let (scroll_tag,scroll_id) = WidgetTag::new();
+        let (scroll_tag, scroll_id) = WidgetTag::new();
         let scroller = Dynamic::new(ScrollController::default());
         let child = Gutter::new(doc.clone(), scroller.clone())
-            .expand_vertically()
-            .width(Px::new(50))//.contain().background_color(Color::new(0x34, 0x3D, 0x46, 0xFF))
+            // .expand_vertically()
+            // .width(Px::new(50))
             .and(
                 MyScroll::new(
-                    TextEditor::new(doc.clone())
-                        .with_scroller(scroller.clone())
-                        ,
+                    TextEditor::new(doc.clone()).with_scroller(scroller.clone()),
                     scroller,
-                ).make_with_tag(scroll_tag)//.contain().background_color(Color::new(0x34, 0x3D, 0x46, 0xFF))
+                )
+                .make_with_tag(scroll_tag) //.contain().background_color(Color::new(0x34, 0x3D, 0x46, 0xFF))
                 .expand(),
             )
-            .into_columns().gutter(Px::new(1)).with(
+            .into_columns()
+            .gutter(Px::new(1))
+            .with(
                 &CornerRadius,
                 CornerRadii::from(Dimension::Lp(Lp::points(0))),
             );
         Self {
             child: child.widget_ref(),
-            scroll_id
+            scroll_id,
         }
     }
 }
@@ -453,18 +522,32 @@ impl WrapperWidget for CodeEditor {
     fn child_mut(&mut self) -> &mut cushy::widget::WidgetRef {
         &mut self.child
     }
-    fn hit_test(&mut self, location: Point<Px>, context: &mut cushy::context::EventContext<'_>) -> bool {
+    fn hit_test(
+        &mut self,
+        _location: Point<Px>,
+        _context: &mut cushy::context::EventContext<'_>,
+    ) -> bool {
         true
     }
 
     fn mouse_wheel(
-            &mut self,
-            device_id: cushy::window::DeviceId,
-            delta: cushy::kludgine::app::winit::event::MouseScrollDelta,
-            phase: cushy::kludgine::app::winit::event::TouchPhase,
-            context: &mut cushy::context::EventContext<'_>,
-        ) -> EventHandling {
-            context.for_other(&self.scroll_id).unwrap().mouse_wheel(device_id, delta, phase);
-            IGNORED
+        &mut self,
+        device_id: cushy::window::DeviceId,
+        delta: cushy::kludgine::app::winit::event::MouseScrollDelta,
+        phase: cushy::kludgine::app::winit::event::TouchPhase,
+        context: &mut cushy::context::EventContext<'_>,
+    ) -> EventHandling {
+        context
+            .for_other(&self.scroll_id)
+            .unwrap()
+            .mouse_wheel(device_id, delta, phase);
+        IGNORED
+    }
+}
+
+define_components! {
+    CodeEditor {
+        TextSize(Lp, "text_size", Lp::points(11))
+        LineHeight(Lp, "line_height", Lp::points(13))
     }
 }
