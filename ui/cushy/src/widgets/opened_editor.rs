5c5d47f709794b50f716e68ea5cb12abebd08e7b
Mathieu PEPONAS <m.peponas@gmail.com>
2024-05-31 10:50:03 +0200

    Can now resize the opened file panel

--- ui/cushy/src/widgets/opened_editor.rs
+++ ui/cushy/src/widgets/opened_editor.rs
@@ -1,10 +1,25 @@
-use cushy::{figures::{units::{Px, UPx}, Point, Size, Zero}, kludgine::{app::winit::platform::windows::Color, text::Text, DrawableExt}, value::{Dynamic, Source}, widget::Widget};
+use cushy::{
+    figures::{
+        units::{Px, UPx},
+        Point, Rect, ScreenScale, Size, Zero,
+    },
+    kludgine::{
+        app::winit::{event::MouseButton, platform::{scancode, windows::Color}},
+        shapes::Shape,
+        text::Text,
+        DrawableExt,
+    },
+    value::{Destination, Dynamic, Source},
+    widget::{Widget, HANDLED, IGNORED},
+};
 use ndoc::Document;
 
 #[derive(Debug)]
 pub struct OpenedEditor {
     documents: Dynamic<Vec<Dynamic<Document>>>,
     current_doc: Dynamic<usize>,
+    width: Dynamic<UPx>,
+    hovered: Dynamic<bool>,
 }
 
 impl OpenedEditor {
@@ -12,7 +27,17 @@ impl OpenedEditor {
         OpenedEditor {
             documents,
             current_doc,
+            width: Dynamic::new(UPx::new(100)),
+            hovered: Dynamic::new(false),
         }
+    }
+
+    fn on_resize_handle(
+        &mut self,
+        location: Point<Px>,
+        context: &mut cushy::context::EventContext<'_>,
+    ) -> bool {
+        location.x > self.width.get().into_px(context.kludgine.scale()) - 5
     }
 }
 
@@ -29,18 +54,89 @@ impl Widget for OpenedEditor {
                 text.push_str(" (current)");
             }
             let text = Text::new(&text, cushy::kludgine::Color::WHITE);
-            context.gfx.draw_text(text.translate_by(Point::new(Px::ZERO, y)));
+            context
+                .gfx
+                .draw_text(text.translate_by(Point::new(Px::ZERO, y)));
             y += 20;
         }
-
+        // TODO: handle is always visible even if not hovered
+        // if self.hovered.get() {
+        //     let width = self.width.get().into_px(context.gfx.scale());
+        //     let scale = context.gfx.scale();
+        //     let height = context.inner_size().get().height.into_px(scale);
+        //     context.gfx.draw_shape(
+        //         Shape::filled_rect(
+        //             Rect::new(
+        //                 Point::new(width - 5, Px::ZERO),
+        //                 Size::new(
+        //                     Px::new(5),
+        //                     height,
+        //                 ),
+        //             ),
+        //             cushy::kludgine::Color::WHITE,
+        //         )
+        //         .translate_by(Point::ZERO),
+        //     );
+        // }
     }
 
     fn layout(
-            &mut self,
-            available_space: cushy::figures::Size<cushy::ConstraintLimit>,
-            context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
-        ) -> cushy::figures::Size<cushy::figures::units::UPx> {
+        &mut self,
+        available_space: cushy::figures::Size<cushy::ConstraintLimit>,
+        context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
+    ) -> cushy::figures::Size<cushy::figures::units::UPx> {
         let h = UPx::new(self.documents.get().len() as _) * 20;
-        Size::new(UPx::new(100), h)
+        Size::new(self.width.get(), h)
+    }
+
+    fn hit_test(
+        &mut self,
+        location: Point<Px>,
+        context: &mut cushy::context::EventContext<'_>,
+    ) -> bool {
+        self.on_resize_handle(location, context)
+    }
+
+    fn hover(
+        &mut self,
+        location: Point<Px>,
+        context: &mut cushy::context::EventContext<'_>,
+    ) -> Option<cushy::kludgine::app::winit::window::CursorIcon> {
+        context.redraw_when_changed(&self.hovered);
+        if self.on_resize_handle(location, context) {
+            dbg!("resize", location, self.width.get());
+            self.hovered.replace(true);
+            Some(cushy::kludgine::app::winit::window::CursorIcon::EwResize)
+        } else {
+            self.hovered.replace(false);
+            None
+        }
+    }
+
+    fn mouse_down(
+        &mut self,
+        location: Point<Px>,
+        device_id: cushy::window::DeviceId,
+        button: MouseButton,
+        context: &mut cushy::context::EventContext<'_>,
+    ) -> cushy::widget::EventHandling {
+        if button == MouseButton::Left && self.on_resize_handle(location, context) {
+            HANDLED
+        } else {
+            IGNORED
+        }
+    }
+
+    fn mouse_drag(
+        &mut self,
+        location: Point<Px>,
+        device_id: cushy::window::DeviceId,
+        button: cushy::kludgine::app::winit::event::MouseButton,
+        context: &mut cushy::context::EventContext<'_>,
+    ) {
+        if button == MouseButton::Left {
+            context.invalidate_when_changed(&self.width);
+            *self.width.lock() = location.x.into_upx(context.kludgine.scale());
+        }
     }
 }
