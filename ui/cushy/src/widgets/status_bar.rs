74502ec75731292fe00b6880e9671288d079f4dc
Mathieu PEPONAS <m.peponas@gmail.com>
2024-12-19 11:02:15 +0100

    Compose Status_bar instead of using custom widget

--- ui/cushy/src/widgets/status_bar.rs
+++ ui/cushy/src/widgets/status_bar.rs
@@ -1,121 +1,101 @@
 use cushy::{
-    figures::{units::Px, IntoSigned, IntoUnsigned, Point, Round, ScreenScale, Size, Zero},
-    kludgine::{
-        shapes::{PathBuilder, StrokeOptions},
-        text::Text,
-        DrawableExt,
-    },
-    styles::{
-        components::{self, FontFamily, LineHeight},
-        Dimension,
-    },
-    value::{Dynamic, Source},
-    widget::Widget,
+    value::{Destination, Dynamic, ForEach, Source},
+    widget::MakeWidget,
+    widgets::Space,
 };
 use ndoc::{Document, Indentation};
 
 #[derive(Debug)]
 pub struct StatusBar {
-    documents: Dynamic<Vec<Dynamic<Document>>>,
-    current_doc: Dynamic<usize>,
+    filename: Dynamic<String>,
+    selection: Dynamic<String>,
+    indent: Dynamic<String>,
+    eol: Dynamic<String>,
+    encoding: Dynamic<String>,
+    syntax: Dynamic<String>,
 }
 
 impl StatusBar {
     pub fn new(documents: Dynamic<Vec<Dynamic<Document>>>, current_doc: Dynamic<usize>) -> Self {
+        let filename = Dynamic::new(String::new());
+        let selection = Dynamic::new(String::new());
+        let indent = Dynamic::new(String::new());
+        let eol = Dynamic::new(String::new());
+        let encoding = Dynamic::new(String::new());
+        let syntax = Dynamic::new(String::new());
+
+        (&documents, &current_doc)
+            .for_each({
+                let filename = filename.clone();
+                let selection = selection.clone();
+                let indent = indent.clone();
+                let eol = eol.clone();
+                let encoding = encoding.clone();
+                let syntax = syntax.clone();
+                move |(d, c)| {
+                    let filename = filename.clone();
+                    let selection = selection.clone();
+                    let indent = indent.clone();
+                    let eol = eol.clone();
+                    let encoding = encoding.clone();
+                    let syntax = syntax.clone();
+                    d[*c]
+                        .for_each(move |doc| {
+                            filename.replace(format!(
+                                "{}{}",
+                                doc.file_name
+                                    .as_ref()
+                                    .map(|f| f.file_name().unwrap().to_string_lossy().into_owned())
+                                    .unwrap_or("Untitled".to_string()),
+                                if doc.is_dirty() { "*" } else { "" }
+                            ));
+                            selection.replace(if doc.selections.len() > 1 {
+                                format!("{} selections", doc.selections.len())
+                            } else {
+                                format!(
+                                    "Ln {}, Col {}",
+                                    doc.selections[0].head.line + 1,
+                                    doc.selections[0].head.column + 1
+                                )
+                            });
+                            indent.replace(match doc.file_info.indentation {
+                                Indentation::Space(s) => format!("Spaces: {}", s),
+                                Indentation::Tab(t) => format!("Tabs: {}", t),
+                            });
+                            eol.replace(match doc.file_info.linefeed {
+                                ndoc::LineFeed::CR => "CR".to_string(),
+                                ndoc::LineFeed::LF => "LF".to_string(),
+                                ndoc::LineFeed::CRLF => "CRLF".to_string(),
+                            });
+                            encoding.replace(doc.file_info.encoding.name().to_string());
+                            syntax.replace(doc.file_info.syntax.name.clone());
+                        })
+                        .persist();
+                }
+            })
+            .persist();
+
         StatusBar {
-            documents: documents.clone(),
-            current_doc: current_doc.clone(),
+            filename,
+            selection,
+            indent,
+            eol,
+            encoding,
+            syntax,
         }
     }
 }
 
-impl Widget for StatusBar {
-    fn redraw(&mut self, context: &mut cushy::context::GraphicsContext<'_, '_, '_, '_>) {
-        context.apply_current_font_settings();
-        let bg = context.get(&components::WidgetBackground);
-        context.fill(bg);
-        let border_color = context.get(&components::OutlineColor);
-        let width = context.gfx.clip_rect().size.width.into_signed();
-        context.gfx.draw_shape(
-            PathBuilder::new(Point::ZERO)
-                .line_to(Point::new(width, Px::ZERO))
-                .build()
-                .stroke(StrokeOptions::px_wide(Px::new(1)).colored(border_color))
-                .translate_by(Point::ZERO),
-        );
-
-        let font_familly = context.get(&FontFamily);
-        if let Some(font_familly) = context.find_available_font_family(&font_familly) {
-            context.gfx.set_font_family(font_familly);
-        }
-
-        let doc = self.documents.get()[self.current_doc.get()].clone();
-        let doc = doc.get();
-        let dirty = if doc.is_dirty() { "*" } else { "" };
-        let s = context.gfx.region();
-
-        let selection = if doc.selections.len() > 1 {
-            format!("{} selections", doc.selections.len())
-        } else {
-            format!(
-                "Ln {}, Col {}",
-                doc.selections[0].head.line + 1,
-                doc.selections[0].head.column + 1
-            )
-        };
-
-        let indent = match doc.file_info.indentation {
-            Indentation::Space(s) => format!("Spaces: {}", s),
-            Indentation::Tab(t) => format!("Tabs: {}", t),
-        };
-
-        let eol = match doc.file_info.linefeed {
-            ndoc::LineFeed::CR => "CR",
-            ndoc::LineFeed::LF => "LF",
-            ndoc::LineFeed::CRLF => "CRLF",
-        };
-
-        let file_name = if let Some(file_name) = doc.file_name {
-            file_name.file_name().unwrap().to_string_lossy().to_string()
-        } else {
-            "Untitled".to_string()
-        };
-        let text = format!("{}{}", file_name, dirty);
-        let text = Text::<Px>::new(&text, cushy::kludgine::Color::WHITE);
-
-        context.gfx.draw_text(text.translate_by(Point::ZERO));
-        let text = format!(
-            "{}  {}  {}  {}  {} ",
-            selection,
-            indent,
-            eol,
-            doc.file_info.encoding.name(),
-            doc.file_info.syntax.name
-        );
-        let text = Text::<Px>::new(&text, cushy::kludgine::Color::WHITE);
-        let mtext = context.gfx.measure_text(text);
-        context
-            .gfx
-            .draw_text(text.translate_by(Point::new(s.size.width - mtext.size.width, Px::ZERO).round()));
-    }
-    fn layout(
-        &mut self,
-        available_space: cushy::figures::Size<cushy::ConstraintLimit>,
-        context: &mut cushy::context::LayoutContext<'_, '_, '_, '_>,
-    ) -> cushy::figures::Size<cushy::figures::units::UPx> {
-        let heigh = match context.get(&LineHeight) {
-            Dimension::Lp(v) => v.into_upx(context.gfx.scale()),
-            Dimension::Px(v) => v.into_unsigned(),
-        };
-
-        // if I don't substract 1 from the width, the layout/redraw is called infinitely, switching between max()+1 and max()
-        Size::new(available_space.width.max() - 1, heigh)
-    }
-    fn hit_test(
-        &mut self,
-        _location: Point<Px>,
-        _context: &mut cushy::context::EventContext<'_>,
-    ) -> bool {
-        true
+impl MakeWidget for StatusBar {
+    fn make_widget(self) -> cushy::widget::WidgetInstance {
+        self.filename
+            .and(Space::clear().expand())
+            .and(self.selection)
+            .and(self.indent)
+            .and(self.eol)
+            .and(self.encoding)
+            .and(self.syntax)
+            .into_columns()
+            .make_widget()
     }
 }
